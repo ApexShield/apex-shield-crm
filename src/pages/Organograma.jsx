157418fffa64c08b936edfe1099e222b134a8d1e
Gustavo Ferreira <apexshieldcorretoradeseguros@gmail.com>
2026-01-24 21:10:28 +0000

    File changes

--- src/pages/Organograma.jsx
+++ src/pages/Organograma.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery, useMutation, useQueryClient } from "@tantml:react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";

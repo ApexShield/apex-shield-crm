a01eebbd98b8bd4bb0c49c2740a5d5b5cd581e67
Gustavo Ferreira <apexshieldcorretoradeseguros@gmail.com>
2026-03-16 04:29:15 +0000

    File changes

--- src/components/campanhas/CampanhaForm.jsx
+++ src/components/campanhas/CampanhaForm.jsx
@@ -304,12 +304,10 @@ export default function CampanhaForm({ open, onClose, clientes = [] }) {
         </div>
       </DialogContent>
     </Dialog>
-
     <WhatsAppLinksDialog
       open={showWhatsappLinks}
       onClose={() => { setShowWhatsappLinks(false); onClose(); }}
       links={whatsappLinks}
     />
-    </>
-  );
+  </>);
 }
